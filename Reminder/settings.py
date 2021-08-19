372118cb8ecfdeaf64d622083804fd0fd1af21d9
Oleksandr Tsapin <otsapin@gmail.com>
2021-08-19 03:25:20 +0300

    REM-12: Add custom user and user manager to authentication.models. Add AUTH_USER_MODEL to Reminder.settings

--- Reminder/settings.py
+++ Reminder/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # New apps
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
