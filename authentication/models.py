7217bce2260518a04a3624de3dc3efd09bc7372b
Oleksandr Tsapin <otsapin@gmail.com>
2021-08-21 00:52:17 +0300

    Remove username from required fields

--- authentication/models.py
+++ authentication/models.py
@@ -2,26 +2,22 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
 
         user = self.model(
             email=self.normalize_email(email),
-            username=username,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password):
+    def create_superuser(self, email, password):
         user = self.create_user(
             email=self.normalize_email(email),
             password=password,
-            username=username,
         )
 
         user.is_admin = True
@@ -41,7 +37,7 @@ class User(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username',]
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
